023282ade7fbb143c80a267c26ca91661a87cf37
Vladimir Kidyaev <krios2146@gmail.com>
2024-08-21 04:36:29 +0700

    feat(view): add HomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import RouterButton from '@/components/RouterButton.vue'
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center gap-y-2">
+    <h1 class="text-4xl text-white">Home</h1>
+    <RouterButton route-name="sign-in">Sign in</RouterButton>
+  </div>
+</template>
