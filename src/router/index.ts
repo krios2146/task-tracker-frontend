cf2dc1c923ad25fc7030efbd5ac989013b84342d
Vladimir Kidyaev <krios2146@gmail.com>
2024-08-21 04:36:38 +0700

    feat(router): update router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import SignInView from '@/views/SignInView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/sign-in', name: 'sign-in', component: SignInView }
+  ]
 })
 
 export default router
