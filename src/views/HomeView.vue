9921ccf7a54599ca0d131dcff6ecf4f0d62f6fd0
Vladimir Kidyaev <krios2146@gmail.com>
2024-08-23 22:19:09 +0700

    feat(auth): update HomeView with displaying username on successful auth

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
+import type IdToken from '@/api/responses/IdToken'
 import RouterButton from '@/components/RouterButton.vue'
+import { useIdToken } from '@/composables/useAuthTokens'
+import { RouteNames } from '@/router/index'
+import { jwtDecode } from 'jwt-decode'
+import type { Ref } from 'vue'
+import { ref } from 'vue'
+
+const username: Ref<string | undefined> = ref()
+
+const { token, found } = useIdToken()
+
+if (found) {
+  const idTokenPayload = jwtDecode<IdToken>(token.value)
+  username.value = idTokenPayload.preferred_username
+}
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center gap-y-2">
-    <h1 class="text-4xl text-white">Home</h1>
-    <RouterButton route-name="sign-in">Sign in</RouterButton>
+  <div class="flex flex-col justify-center items-center gap-y-2 text-white">
+    <h1 class="text-4xl">Home</h1>
+    <p v-if="username">Hello, {{ username }}</p>
+    <RouterButton :route-name="RouteNames.SIGN_IN">Sign in</RouterButton>
   </div>
 </template>
