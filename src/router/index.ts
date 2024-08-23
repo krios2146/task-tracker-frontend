6ccb86aa37b8897eee6844257a8552ec979f9396
Vladimir Kidyaev <krios2146@gmail.com>
2024-08-23 22:18:46 +0700

    feat(router): update routes mappings & add enum for route names

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import SignInView from '@/views/SignInView.vue'
+import SignInView from '@/views/auth/SignInView.vue'
+import KeycloakView from '@/views/auth/KeycloakView.vue'
 import HomeView from '@/views/HomeView.vue'
+
+export const enum RouteNames {
+  HOME = 'home',
+  SIGN_IN = 'sign-in',
+  KEYCLOAK_AUTH = 'keycloak-auth'
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/sign-in', name: 'sign-in', component: SignInView }
+    { path: '/', name: RouteNames.HOME, component: HomeView },
+    { path: '/auth/sign-in', name: RouteNames.SIGN_IN, component: SignInView },
+    {
+      path: '/auth/keycloak/callback',
+      name: RouteNames.KEYCLOAK_AUTH,
+      component: KeycloakView
+    }
   ]
 })
 
