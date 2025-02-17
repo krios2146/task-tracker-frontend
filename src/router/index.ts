76e9b6add5f89f9df1b54d13644a06ab0457f7a2
Vladimir Kidyaev <krios2146@gmail.com>
2025-02-17 23:18:42 +0700

    feat(router): add /board route to the router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,22 +2,37 @@ import { createRouter, createWebHistory } from 'vue-router'
 import SignInView from '@/views/auth/SignInView.vue'
 import KeycloakView from '@/views/auth/KeycloakView.vue'
 import HomeView from '@/views/HomeView.vue'
+import BoardView from '@/views/BoardView.vue'
 
 export const enum RouteNames {
   HOME = 'home',
   SIGN_IN = 'sign-in',
-  KEYCLOAK_AUTH = 'keycloak-auth'
+  KEYCLOAK_AUTH = 'keycloak-auth',
+  BOARD = 'board'
 }
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: RouteNames.HOME, component: HomeView },
-    { path: '/auth/sign-in', name: RouteNames.SIGN_IN, component: SignInView },
+    {
+      path: '/',
+      name: RouteNames.HOME,
+      component: HomeView
+    },
+    {
+      path: '/auth/sign-in',
+      name: RouteNames.SIGN_IN,
+      component: SignInView
+    },
     {
       path: '/auth/keycloak/callback',
       name: RouteNames.KEYCLOAK_AUTH,
       component: KeycloakView
+    },
+    {
+      path: '/board',
+      name: RouteNames.BOARD,
+      component: BoardView
     }
   ]
 })
