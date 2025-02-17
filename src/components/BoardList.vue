eb81b9c5ea97f1b53d3d13429e4d024f631db29f
Vladimir Kidyaev <krios2146@gmail.com>
2025-02-17 23:23:18 +0700

    feat(board): add BoardList component

---
+++ src/components/BoardList.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+console.log('')
+</script>
+
+<template>
+  <div>Board List</div>
+</template>
--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import BoardList from '@/components/BoardList.vue'
 console.log('')
 </script>
 
 <template>
+  <BoardList />
   <div>Board View</div>
 </template>
