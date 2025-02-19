1fd70a0e749d59b9ad22a8511adf84ca5d18e1b5
Vladimir Kidyaev <krios2146@gmail.com>
2025-02-19 22:29:47 +0700

    feat(board): add tasks hover effect

--- src/components/BoardList.vue
+++ src/components/BoardList.vue
@@ -7,8 +7,7 @@ const tasks = ref([
   'Test task one very long title which continues to another line and even further',
   'Test task two',
   'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam aut voluptas numquam quam totam sequi! Praesentium vitae assumenda dolor eaque nesciunt, velit illum expedita minima omnis beatae molestias consectetur. Blanditiis?',
-  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam aut voluptas numquam quam totam sequi! Praesentium vitae assumenda dolor eaque nesciunt, velit illum expedita minima omnis beatae molestias consectetur. Blanditiis?',
-  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam aut voluptas numquam quam totam sequi! Praesentium vitae assumenda dolor eaque nesciunt, velit illum expedita minima omnis beatae molestias consectetur. Blanditiis?'
+  'OCaml is the best programming language ever created, Java is the worst, argue with me on that for you own risk'
 ])
 </script>
 
@@ -18,7 +17,7 @@ const tasks = ref([
       <slot></slot>
     </h2>
 
-    <div class="flex flex-col gap-y-2 overflow-y-scroll pb-1 scrollbar-hidden">
+    <div class="flex flex-col gap-y-2 overflow-y-scroll p-0.5 scrollbar-hidden">
       <BoardListTask v-for="task in tasks" :key="task">{{ task }} </BoardListTask>
     </div>
 
--- src/components/BoardListTask.vue
+++ src/components/BoardListTask.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="p-2 px-3 rounded-md bg-gray-950 shadow-xs shadow-black hover:">
+  <div
+    class="p-2 px-3 rounded-md bg-gray-950 shadow-xs shadow-black hover:cursor-pointer hover:ring-blue-500 hover:ring"
+  >
     <p class="text-gray-200 select-none">
       <slot></slot>
     </p>
